3e795e1350c3fe175a6c488e62f36ecfbd33ab37
somoye123 <somoye.ayotunde@gmail.com>
2021-02-23 21:04:57 +0100

    add routes to server file

--- server.js
+++ server.js
@@ -43,6 +43,9 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to bezkoder application.' });
 });
 
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
