1f76641f1af51055ff4d17cbfab4670cb0cd88cc
somoye123 <somoye.ayotunde@gmail.com>
2021-02-23 20:35:58 +0100

    call sequelize sync function and create 3 role rows

--- server.js
+++ server.js
@@ -2,7 +2,30 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const db = require('./models');
+const Role = db.role;
 
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: 'user',
+  });
+
+  Role.create({
+    id: 2,
+    name: 'moderator',
+  });
+
+  Role.create({
+    id: 3,
+    name: 'admin',
+  });
+}
 var corsOptions = {
   origin: 'http://localhost:8081',
 };
